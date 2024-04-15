aab55fc8d5a9ddf2fc8d4e5fdda90974a964d3a8
Ajay Raj Singh <84695140+Ajayraj9001@users.noreply.github.com>
2024-04-15 11:01:19 +0000

    1st Program Done

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,10 @@
 console.log("Ajay Raj Singh")
+
+let S_number = 1234
+let S_name = "Ajay Raj"
+let S_email = "ajaypratap@example.com"
+
+const var_Id = 32445
+
+console.log(S_number)
+console.table([S_number, S_name, S_email, var_Id])
