5a94362ec5fc717b3a33ef5b77e82f9a18893c6a
Ajay Raj Singh <84695140+Ajayraj9001@users.noreply.github.com>
2024-05-01 13:36:38 +0000

    functions

--- 04_Basics/01_functions.js
+++ 04_Basics/01_functions.js
@@ -11,5 +11,12 @@ sayName    // This is called function Reference
 
 sayName()   // This is called function execution(Calling)
 
+function addTwoNumbers(number1, number2){     // At the time of function defenition after function name inside paranteses '()' anything we write is called parameter
+
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(3, 1)    // At the time of calling a function we give some values in Parenteses '()' are called arguments
+
 
 
