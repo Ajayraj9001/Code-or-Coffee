833d36f9f70a2784913d4cfc9976c2da9fa6f3a0
Ajay Raj Singh <84695140+Ajayraj9001@users.noreply.github.com>
2024-05-01 13:17:14 +0000

    Functions going on

---
+++ 04_Basics/01_functions.js
@@ -0,0 +1,15 @@
+// Functions in Js
+
+function sayName(){
+    console.log("A");
+    console.log("j");
+    console.log("a");
+    console.log("y");
+}
+
+sayName    // This is called function Reference
+
+sayName()   // This is called function execution(Calling)
+
+
+
