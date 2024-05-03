ea9b36c7b52d35ec95313c0ca0557cce1bb952ff
Ajay Raj Singh <84695140+Ajayraj9001@users.noreply.github.com>
2024-05-03 10:48:33 +0000

    Scope is going on

---
+++ 05_Basics/01_Scopes.js
@@ -0,0 +1,20 @@
+// Scops:- Block scope, Global scope, Function scope
+let name = "Ajay"    
+const id = 123
+var b = 100
+
+if(true) {
+    console.log(b);    
+}
+
+console.log(name);
+console.log(id);
+
+// Note:- Try not to us 'var' classical variable to store data in JS, instead use let and const 
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+
